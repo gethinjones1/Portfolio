eae281ee560685da961ce99eca23b4517270c690
gethinjones1 <gethinjackjones@gmail.com>
2021-03-25 23:07:20 +0000

    20210325 updates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import Particles from "react-particles-js";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+
 
 
 function App() {
   return (
-    <h2>Portfolio</h2>
+  <>
+    <Particles 
+      params={{
+        particles: {
+          number: {
+            value: 30,
+            density: {
+              enable: true,
+              value_area: 900,
+            }
+          },
+          shape: {
+            type: "circle",
+            stroke: {
+              width: 6,
+              color: "#03A062"  
+            }
+          }
+        }
+      }}
+    />
+    <Navbar />
+    <Header />
+  </>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from "react"
+import Typed from "react-typed"
+
+const Header = () => {
+    return (
+        <div className="header-wrapper">
+            <div className="main-info">
+                <h1></h1>
+                <Typed
+                    className="type-text"
+                    strings={["Gethin Jones", "22 Years Old", "Aspiring Software Engineer"]}
+                    typeSpeed={40}
+                    backSpeed={40}
+                    loop
+                />
+                <a href="#" className="btn-main-offer">Contact Me</a>
+            </div>           
+        </div>
+    )
+}
+
+export default Header
