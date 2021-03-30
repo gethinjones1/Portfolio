a0c237b9b45d86e844c9ac0b14272ba7fb7bf0a9
gethinjackjones <80970965+gethinjones1@users.noreply.github.com>
2021-03-30 11:02:59 +0100

    20210330

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import 'react-mdl/extra/material.css';
+import 'react-mdl/extra/material.js';
 
 ReactDOM.render(
 
